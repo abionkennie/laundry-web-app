75cb69652a59c4ebd2281d0553c8075057ecca41
abionkennie <80458135+abionkennie@users.noreply.github.com>
2022-02-21 17:12:02 +0100

    add: orderform

--- js/laundry.js
+++ js/laundry.js
@@ -8,14 +8,14 @@ let current = 1;
 function tab2() {
     form.style.marginLeft = "-100%";
     login.style.background = "none";
-    signup.style.background = "linear-gradient(45deg, #eecda7, #fc7a00)";
+    signup.style.background = "linear-gradient(45deg, #eecda7,var(--blue))";
     switches[current - 1].classList.add("active");
 }
 
 function tab1() {
     form.style.marginLeft = "0";
     signup.style.background = "none";
-    login.style.background = "linear-gradient(45deg, #eecda7, #fc7a00)";
+    login.style.background = "linear-gradient(45deg, #eecda7, var(--orange))";
     switches[current - 1].classList.remove("active");
 
 }
