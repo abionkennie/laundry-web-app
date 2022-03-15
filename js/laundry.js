cbe6909b4ff046b91e310791e45b1ad68c975cd7
abionkennie <80458135+abionkennie@users.noreply.github.com>
2022-03-15 13:09:26 +0100

    add: services

--- js/laundry.js
+++ js/laundry.js
@@ -2,6 +2,7 @@ const login = document.querySelector(".login");
 const signup = document.querySelector(".signup");
 const form = document.querySelector("#form");
 const switches = document.querySelectorAll(".switch");
+const Navigation = document.querySelector(".navigation");
 
 let current = 1;
 
@@ -17,5 +18,26 @@ function tab1() {
     signup.style.background = "none";
     login.style.background = "linear-gradient(45deg, #eecda7, var(--orange))";
     switches[current - 1].classList.remove("active");
-
 }
+
+function showDropdown() {
+    $dropdownContent = $(".dropdown-content").css("display");
+    if ($dropdownContent === "flex") {
+        $(".dropdown-content").css("display", "none");
+    } else {
+        $(".dropdown-content").css({ "display": "flex", "flex-direction": "column" });
+    }
+}
+
+function closeMe() {
+    $dropdownContent = $(".dropdown-content").css("display");
+    if ($dropdownContent === "flex") {
+        $(".dropdown-content").css("display", "none");
+    }
+}
+
+// document.querySelector(".navbar ul li:nth-child(2)").addEventListener("click",
+//     function() {
+//         this.classList.add(".subnav");
+
+//     });
--- js/orderform.js
+++ js/orderform.js
@@ -1,31 +1,30 @@
-const container = document.querySelector('.container');
-const orderForm = document.querySelectorAll('#form');
-const button = document.querySelector('.btn1');
+const formEl = document.querySelector('form')
+const tableElement = document.querySelector('.table-element');
+const tbodyEl = document.querySelector('tbody');
+const tableEl = document.querySelector('#content-display');
 
-var list1 = [];
-var list2 = [];
-var list3 = [];
-var n = 1;
-var x = 0;
-
-function AddRow() {
-    var AddRown = document.getElementById('#content-display');
-    var NewRow = AddRown.insertRow(n);
-
-    list1[x] = document.getElementById('Type of Cloth').value;
-    list2[x] = document.getElementById('How many').value;
-    list3[x] = document.getElementById('').value;
-
-    var cel1 = NewRow.insertCell(0);
-    var cel2 = NewRow.insertCell(1);
-    var cel3 = NewRow.insertCell(2);
-    cel1.innerHTML = list1[x];
-    cel2.innerHTML = list2[x];
-    cel3.innerHTML = list3[x];
-
-    n++;
-    x++;
-
-
-
+function onAddWebsite(e) {
+    e.preventDefault();
+    const fabric = document.getElementById("select").value;
+    const quantity = document.getElementById("quantity").value;
+    tbodyEl.innerHTML += `
+    <tr>
+     <td>${fabric}</td>
+     <td>${quantity}</td>
+     <td><button class="deleteBtn">X</button></td>
+    </tr>
+     `
 }
+
+
+function onDeleteRow(e) {
+    if (!e.target.classList.contains("deleteBtn")) {
+        return;
+    }
+    const btn = e.target;
+    btn.closest("tr").remove();
+}
+
+
+formEl.addEventListener('submit', onAddWebsite);
+tableEl.addEventListener("click", onDeleteRow);
